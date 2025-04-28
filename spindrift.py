a1dd0cef38c2c93260b56269086bef58d496b191
loverofspindrift <dylandrift480@gmail.com>
2025-04-28 15:12:20 -0700

    Create spindrift.py

---
+++ spindrift.py
@@ -0,0 +1,12 @@
+import random
+
+spindrifts = [
+  "Fuji Apple",
+  "Lemonade",
+  "Lime",
+  "Raspberry",
+  "Half Tea & Half Lemon"
+]
+
+if __name__ == "__main__":
+  print(random.choice(spindrifts))
